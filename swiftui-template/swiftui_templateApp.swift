372d82e8f8c6376de19ff43fa7819baaa2929f37
Kyle Nathan Naranjo <63973445+teslakoile@users.noreply.github.com>
2023-09-09 17:35:40 +0800

    Initial Commit

---
+++ swiftui-template/swiftui_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_templateApp.swift
+//  swiftui-template
+//
+//  Created by Kyle Naranjo on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
